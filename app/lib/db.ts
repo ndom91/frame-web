27bb9b23e27146a4cfb25b2413d5763fce31ba79
ndom91 <yo@ndo.dev>
2025-05-28 21:32:12 +0200

    feat: add drizzle-orm

---
+++ app/lib/db.ts
@@ -0,0 +1,10 @@
+import { drizzle } from 'drizzle-orm/libsql/node';
+
+const db = drizzle({
+  connection: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!
+  }
+});
+
+export { db }
