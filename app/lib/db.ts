d881b91dbfb45bb19280e145cb4bab683019686f
ndom91 <yo@ndo.dev>
2025-07-28 22:32:44 +0200

    Add schema to drizzle init

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,8 +1,26 @@
-import { drizzle } from 'drizzle-orm/libsql';
+import { drizzle } from "drizzle-orm/libsql";
+import { user } from "../../db/user.sql";
+import { frame } from "../../db/frame.sql";
+import { usersToFrames } from "../../db/frameOnUser.sql";
+import { media } from "../../db/media.sql";
+import { verification } from "../../db/verification.sql";
+import { session } from "../../db/session.sql";
+import { account } from "../../db/account.sql";
+import { passkey as passkeySchema } from "../../db/passkey.sql";
 
 export const db = drizzle({
-  connection: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!
-  }
+	schema: {
+		user,
+		verification,
+		session,
+		account,
+		passkey: passkeySchema,
+		frame,
+		usersToFrames,
+		media,
+	},
+	connection: {
+		url: process.env.TURSO_DATABASE_URL!,
+		authToken: process.env.TURSO_AUTH_TOKEN!,
+	},
 });
--- app/lib/metadata.ts
+++ app/lib/metadata.ts
@@ -1,87 +1,88 @@
-import type { Metadata } from "next"; import type { Viewport } from 'next'
+import type { Metadata } from "next";
+import type { Viewport } from "next";
 
 export const viewport: Viewport = {
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
-  ],
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-  colorScheme: 'dark light',
-}
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
+	colorScheme: "dark light",
+};
 
 export const metadata: Metadata = {
-  title: "Domino Frame",
-  description: "Manage your Frames",
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
-  },
-  robots: {
-    index: false,
-    follow: false,
-    nocache: false,
-    googleBot: {
-      index: false,
-      follow: false,
-      noimageindex: false,
-    },
-  },
-  openGraph: {
-    title: 'Domino Frame',
-    description: 'Manage your Frames',
-    url: 'https://dominoframe.vercel.app',
-    siteName: 'Domino Frame',
-    images: [
-      {
-        url: 'https://nextjs.org/og.png', // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
-        width: 1800,
-        height: 1600,
-        alt: 'My custom alt',
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-  // icons: {
-  //   icon: [
-  //     { url: '/icon.png' },
-  //     new URL('/icon.png', 'https://example.com'),
-  //     { url: '/icon-dark.png', media: '(prefers-color-scheme: dark)' },
-  //   ],
-  //   shortcut: ['/shortcut-icon.png'],
-  //   apple: [
-  //     { url: '/apple-icon.png' },
-  //     { url: '/apple-icon-x3.png', sizes: '180x180', type: 'image/png' },
-  //   ],
-  //   other: [
-  //     {
-  //       rel: 'apple-touch-icon-precomposed',
-  //       url: '/apple-touch-icon-precomposed.png',
-  //     },
-  //   ],
-  // },
-  // itunes: {
-  //   appId: 'myAppStoreID',
-  //   appArgument: 'myAppArgument',
-  // },
-  // appleWebApp: {
-  //   title: 'Apple Web App',
-  //   statusBarStyle: 'black-translucent',
-  //   startupImage: [
-  //     '/assets/startup/apple-touch-startup-image-768x1004.png',
-  //     {
-  //       url: '/assets/startup/apple-touch-startup-image-1536x2008.png',
-  //       media: '(device-width: 768px) and (device-height: 1024px)',
-  //     },
-  //   ],
-  // },
+	title: "Domino Frame",
+	description: "Manage your Frames",
+	formatDetection: {
+		email: false,
+		address: false,
+		telephone: false,
+	},
+	robots: {
+		index: false,
+		follow: false,
+		nocache: false,
+		googleBot: {
+			index: false,
+			follow: false,
+			noimageindex: false,
+		},
+	},
+	openGraph: {
+		title: "Domino Frame",
+		description: "Manage your Frames",
+		url: "https://dominoframe.vercel.app",
+		siteName: "Domino Frame",
+		images: [
+			{
+				url: "https://nextjs.org/og.png", // Must be an absolute URL
+				width: 800,
+				height: 600,
+			},
+			{
+				url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
+				width: 1800,
+				height: 1600,
+				alt: "My custom alt",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	icons: {
+		icon: [
+			{ url: "/logo_transparent.png" },
+			// new URL('/icon.png', 'https://example.com'),
+			// { url: '/icon-dark.png', media: '(prefers-color-scheme: dark)' },
+		],
+		// shortcut: ['/shortcut-icon.png'],
+		// apple: [
+		//   { url: '/apple-icon.png' },
+		//   { url: '/apple-icon-x3.png', sizes: '180x180', type: 'image/png' },
+		// ],
+		// other: [
+		//   {
+		//     rel: 'apple-touch-icon-precomposed',
+		//     url: '/apple-touch-icon-precomposed.png',
+		//   },
+		// ],
+	},
+	// itunes: {
+	//   appId: 'myAppStoreID',
+	//   appArgument: 'myAppArgument',
+	// },
+	// appleWebApp: {
+	//   title: 'Apple Web App',
+	//   statusBarStyle: 'black-translucent',
+	//   startupImage: [
+	//     '/assets/startup/apple-touch-startup-image-768x1004.png',
+	//     {
+	//       url: '/assets/startup/apple-touch-startup-image-1536x2008.png',
+	//       media: '(device-width: 768px) and (device-height: 1024px)',
+	//     },
+	//   ],
+	// },
 };
