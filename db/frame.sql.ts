ef184473c6c0da52420b09c63b4598c0b2ca935d
ndom91 <yo@ndo.dev>
2025-07-31 00:23:51 +0200

    Update frame schema

--- db/frame.sql.ts
+++ db/frame.sql.ts
@@ -3,12 +3,20 @@ import { int, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { usersToFrames } from "./frameOnUser.sql";
 
 export const frame = sqliteTable("frame", {
-  id: int().primaryKey({ autoIncrement: true }),
-  title: text().notNull(),
-  createdAt: integer('created_at', { mode: 'timestamp' }).$defaultFn(() => /* @__PURE__ */ new Date()).notNull(),
-  updatedAt: integer('updated_at', { mode: 'timestamp' }).$defaultFn(() => /* @__PURE__ */ new Date()).notNull()
+	id: int().primaryKey({ autoIncrement: true }),
+	title: text().notNull(),
+	frameId: text().notNull(),
+	location: text().notNull(),
+	model: text().notNull(),
+	status: text().$type<"online" | "offline" | "syncing">().default("offline"),
+	createdAt: integer("created_at", { mode: "timestamp" })
+		.$defaultFn(() => /* @__PURE__ */ new Date())
+		.notNull(),
+	updatedAt: integer("updated_at", { mode: "timestamp" }).$defaultFn(
+		() => /* @__PURE__ */ new Date(),
+	),
 });
 
 export const usersRelations = relations(frame, ({ many }) => ({
-  usersToFrames: many(usersToFrames)
+	usersToFrames: many(usersToFrames),
 }));
