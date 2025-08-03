046a714bc87530b7143195da57a00286dc984882
ndom91 <yo@ndo.dev>
2025-08-03 22:50:42 +0200

    Add google social provider

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -2,8 +2,8 @@ import { createAuthClient } from "better-auth/react";
 import { passkeyClient } from "better-auth/client/plugins";
 
 export const { signIn, signOut, useSession, passkey } = createAuthClient({
-	baseURL: process.env.VERCEL_URL
-		? `https://${process.env.VERCEL_URL}`
+	baseURL: process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+		? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
 		: "http://localhost:3000",
 	plugins: [passkeyClient()],
 });
