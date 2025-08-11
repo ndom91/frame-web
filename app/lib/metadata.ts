7ccd376d156ac6c98bad21871c8b19d215a1a698
ndom91 <yo@ndo.dev>
2025-08-11 19:33:27 +0200

    Update metadata for apple

--- app/lib/metadata.ts
+++ app/lib/metadata.ts
@@ -34,55 +34,32 @@ export const metadata: Metadata = {
 	openGraph: {
 		title: "Domino Frame",
 		description: "Manage your Frames",
-		url: "https://dominoframe.vercel.app",
+		url: "https://domino.photos",
 		siteName: "Domino Frame",
 		images: [
 			{
-				url: "https://nextjs.org/og.png", // Must be an absolute URL
-				width: 800,
-				height: 600,
-			},
-			{
-				url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
-				width: 1800,
-				height: 1600,
-				alt: "My custom alt",
+				url: "https://domino.photos/logo.png", // Must be an absolute URL
+				width: 256,
+				height: 256,
 			},
 		],
 		locale: "en_US",
 		type: "website",
 	},
-	// icons: {
-	// 	icon: [
-	// { url: "/logo.png" },
-	// new URL('/icon.png', 'https://example.com'),
-	// { url: '/icon-dark.png', media: '(prefers-color-scheme: dark)' },
-	// ],
-	// shortcut: ['/shortcut-icon.png'],
-	// apple: [
-	//   { url: '/apple-icon.png' },
-	//   { url: '/apple-icon-x3.png', sizes: '180x180', type: 'image/png' },
-	// ],
-	// other: [
-	//   {
-	//     rel: 'apple-touch-icon-precomposed',
-	//     url: '/apple-touch-icon-precomposed.png',
-	//   },
-	// ],
-	// },
-	// itunes: {
-	//   appId: 'myAppStoreID',
-	//   appArgument: 'myAppArgument',
-	// },
-	// appleWebApp: {
-	//   title: 'Apple Web App',
-	//   statusBarStyle: 'black-translucent',
-	//   startupImage: [
-	//     '/assets/startup/apple-touch-startup-image-768x1004.png',
-	//     {
-	//       url: '/assets/startup/apple-touch-startup-image-1536x2008.png',
-	//       media: '(device-width: 768px) and (device-height: 1024px)',
-	//     },
-	//   ],
-	// },
+	icons: {
+		icon: [{ url: "/logo.png" }],
+		shortcut: ["/logo.png"],
+		apple: [{ url: "/logo.png" }],
+		other: [
+			{
+				rel: "apple-touch-icon-precomposed",
+				url: "/apple-touch-icon-precomposed.png",
+			},
+		],
+	},
+	appleWebApp: {
+		title: "Domino Frame",
+		statusBarStyle: "black-translucent",
+		startupImage: ["/logo.png"],
+	},
 };
